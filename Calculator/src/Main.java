a76c0db65e89b1f1589121b400a64aa6f0907aee
Latheesh Mahendran <latheeshmahendran@gmail.com>
2024-10-23 01:35:25 +0530

    Calculator- Completed

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,15 +1,72 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void Cal(){
+        Scanner getnum = new Scanner(System.in);
+        double num1, num2, func;
+
+        System.out.println("-------------------------------------------------------");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Welcome to simple Calculator, Enter your first number!");
+        num1 = getnum.nextDouble();
+        System.out.println(num1+" is your first number, Enter your second number!");
+        num2 = getnum.nextDouble();
+
+        System.out.println("Specify the Arithmetic operation you need to perform for "+num1+" and "+num2+" by entering any one of the number given below:");
+        System.out.println("Addition  ---- 0 ");
+        System.out.println("Subtraction  ---- 1 ");
+        System.out.println("Multiplication  ---- 2 ");
+        System.out.println("Division  ---- 3 ");
+        System.out.println("Exit  ---- 4 ");
+        System.out.println("--- --- --- Enter Number Below --- --- ---");
+
+        func = getnum.nextDouble();
+
+        switch ((int) func){
+            case 0:
+                Add(num1, num2);
+                break;
+            case 1:
+                Sub(num1, num2);
+                break;
+            case 2:
+                Mul(num1, num2);
+                break;
+            case 3:
+                Div(num1, num2);
+                break;
+            case 4:
+                System.exit(0);
+                break;
         }
+    }
+    public static void Add(double num1, double num2){
+        num1 += num2;
+        System.out.println(num1+" is the total");
+        Cal();
+    }
+    public static void Sub(double num1, double num2){
+        num1 -= num2;
+        System.out.println(num1+" is the total");
+        Cal();
+    }
+    public static void Mul(double num1, double num2){
+        num1 *= num2;
+        System.out.println(num1+" is the total");
+        Cal();
+    }
+    public static void Div(double num1, double num2){
+        num1 /= num2;
+        System.out.println(num1+" is the total");
+        Cal();
+    }
+
+    public static void main(String[] args) {
+        Cal();
     }
 }
