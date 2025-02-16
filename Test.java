843cd4b6eb1160b5dd31dcc00911edec0444f4c3
Latheesh Mahendran <latheeshmahendran@gmail.com>
2025-02-16 13:53:49 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        
+        // int arr = [23,90,45,34,22];
+        int [] arr = {23,90,45,34,22};
+        int i = 4;
+        for (i=4; i >= 0; i--){
+            System.out.println(arr[i]);
+        }
+    }
+}
